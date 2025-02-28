4fa5407b67e972d590c0251a51effdb653753e7d
Hussein-02 <hussein02ab@gmail.com>
2025-02-28 17:26:37 +0200

    finished login.php

---
+++ wallet-server/user/v1/login.php
@@ -0,0 +1,42 @@
+<?php
+
+include "connection/connection.php";
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $sql = "SELECT user_id, email, phone, password_hash, role, username FROM userdata WHERE email = ? or username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss",$email,$username);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if($stmt->num_rows>0){
+        $stmt->bind_result($id,$email,$phone,$hashed_password,$role,$username);
+        $stmt->fetch();
+
+        if(password_verify($password,$hashed_password)){
+            session_start();
+            $_SESSION["user_id"]=$id;
+            $_SESSION["email"]=$email;
+            $_SESSION["phone"]=$phone;
+            $_SESSION["role"]=$role;
+            $_SESSION["username"]=$username;
+            
+            echo "login successfull!";
+        }else{
+            echo "incorrect password";
+        }
+
+    }else{
+        echo "email or username not found";
+    }
+    $stmt->close();
+    $conn->close();
+
+}
+
+
+?>
