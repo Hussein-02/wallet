388d10f40eabbccea8c168c916ecbd0ad0635615
Hussein-02 <hussein02ab@gmail.com>
2025-03-05 15:01:55 +0200

    fixed register api

--- wallet-server/connection/connection.php
+++ wallet-server/connection/connection.php
@@ -11,4 +11,7 @@ if ($conn->connect_error) {
     die("connection failed:" . $conn->connect_error);
 }
 
-// echo "Database connected successfully!"
+//uncomment if cors problem
+// header('Access-Control-Allow-Origin:http://127.0.0.1:5500');
+// header('Access-Control-Allow-Methods:POST,GET,OPTIONS');
+// header('Access-Control-Allow-Headers: Content-Type,Authorization');
--- wallet-server/user/v1/register.php
+++ wallet-server/user/v1/register.php
@@ -4,6 +4,27 @@ include "../../connection/connection.php";
 include_once "../../utils.php";
 include "../../models/User.php";
 
+$key = "12345";
+
+function base64url_encode($data)
+{
+    return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+}
+
+function generate_jwt($payload, $key)
+{
+    $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+    $header_encoded = base64url_encode($header);
+
+    $payload_encoded = base64url_encode(json_encode($payload));
+
+    $signature = hash_hmac('sha256', "$header_encoded.$payload_encoded", $key, true);
+    $signature_encoded = base64url_encode($signature);
+
+    return "$header_encoded.$payload_encoded.$signature_encoded";
+}
+
+
 $usermodel = new User($conn);
 
 
@@ -31,7 +52,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($checkUniqueStmt->num_rows > 0) {
         return_failure("username,phone number or email already exists");
     } else {
-        $usermodel->createUser($username, $email, $phone, $password);
+        if ($usermodel->createUser($username, $email, $phone, $password)) {
+            $user_id = $conn->insert_id;
+
+            $token_payload = [
+                "user_id" => $user_id,
+                "email" => $email,
+                "username" => $username,
+                "role" => "user",
+                "exp" => time() + 60 * 60
+            ];
+
+            $token = generate_jwt($token_payload, $key);
+
+            echo json_encode([
+                "success" => true,
+                "message" => "Registration successful",
+                "token" => $token,
+                "user" => [
+                    "id" => $user_id,
+                    "email" => $email,
+                    "username" => $username,
+                    "role" => "user"
+                ]
+
+            ]);
+        } else {
+            return_failure("error registering user");
+        }
     }
     $checkUniqueStmt->close();
     $conn->close();
